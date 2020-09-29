1b96a12d1a88e003dc1696b460d5351eb6609974
Alina Balysh <lina.balysh@gmail.com>
2020-09-29 14:24:56 +0300

    feat: add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0)
+        return 0;
+    else {
+        let minValue = Number.MAX_VALUE;
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] < minValue)
+                minValue = array[i];
+        }
+        return minValue;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0)
+        return 0;
+    else {
+        let maxValue = Number.MIN_VALUE;
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > maxValue)
+                maxValue = array[i];
+        }
+        return maxValue;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0)
+        return 0;
+    else {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum / array.length;
+    }
 }
